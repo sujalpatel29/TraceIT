562333d9b1ccb0d21b86e0a88b9308714ddcdd2b
Sujal Patel <101589553+sujalpatel29@users.noreply.github.com>
2025-05-04 21:35:29 +0530

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,15 @@
+<nav class="nav">
+        <div class="logo">
+            <i class="fa-brands fa-searchengin"></i>
+            <h1 class="title">TraceIT</h1>
+        </div>
+        <div class="center-nav">
+            <a href="index.php" class="list-nav-item">Home</a>
+            <a href="lost.php" class="list-nav-item">Lost</a>
+            <a href="found.php" class="list-nav-item">Found</a>
+            <a href="contact.php" class="list-nav-item">Contact</a>
+        </div>
+        <a href="login.php" class="right-nav">
+            <i class="fa-solid fa-user"></i>
+        </a>
+    </nav>
